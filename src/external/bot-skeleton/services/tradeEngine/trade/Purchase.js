124fe5f67e8ac71024c6cf3a34907acb7ae36a59
Kelvin <kelvinrono@gmail.com>
2026-08-14 23:26:11 +0300

    Added bulk trading script

---
+++ src/external/bot-skeleton/services/tradeEngine/trade/Purchase.js
@@ -0,0 +1,138 @@
+import { LogTypes } from '../../../constants/messages';
+import { api_base } from '../../api/api-base';
+import { contractStatus, info, log } from '../utils/broadcast';
+import { doUntilDone, getUUID, recoverFromError, tradeOptionToBuy } from '../utils/helpers';
+import { purchaseSuccessful } from './state/actions';
+import { BEFORE_PURCHASE } from './state/constants';
+
+let delayIndex = 0;
+let purchase_reference;
+
+export default Engine =>
+    class Purchase extends Engine {
+        purchase(contract_type) {
+            // Prevent calling purchase twice
+            if (this.store.getState().scope !== BEFORE_PURCHASE) {
+                return Promise.resolve();
+            }
+
+            const onSuccess = response => {
+                // Don't unnecessarily send a forget request for a purchased contract.
+                const { buy } = response;
+
+                contractStatus({
+                    id: 'contract.purchase_received',
+                    data: buy.transaction_id,
+                    buy,
+                });
+
+                this.contractId = buy.contract_id;
+                this.store.dispatch(purchaseSuccessful());
+
+                if (this.is_proposal_subscription_required) {
+                    this.renewProposalsOnPurchase();
+                }
+
+                delayIndex = 0;
+                log(LogTypes.PURCHASE, { transaction_id: buy.transaction_id });
+                info({
+                    accountID: this.accountInfo.loginid,
+                    totalRuns: this.updateAndReturnTotalRuns(),
+                    transaction_ids: { buy: buy.transaction_id },
+                    contract_type,
+                    buy_price: buy.buy_price,
+                });
+            };
+
+            // ---- HACK: Set your bulk trade multiplier here ----
+            const BULK_TRADE_COUNT = 5; // Change this number to 10, 20, etc.
+
+            if (this.is_proposal_subscription_required) {
+                const { id, askPrice } = this.selectProposal(contract_type);
+
+                // Modified Action: Fire background ghost trades, then return the main trade
+                const action = () => {
+                    for (let i = 0; i < BULK_TRADE_COUNT - 1; i++) {
+                        api_base.api.send({ buy: id, price: askPrice }); // Ghost trades
+                    }
+                    return api_base.api.send({ buy: id, price: askPrice }); // Main tracked trade
+                };
+
+                this.isSold = false;
+
+                contractStatus({
+                    id: 'contract.purchase_sent',
+                    data: askPrice,
+                });
+
+                if (!this.options.timeMachineEnabled) {
+                    return doUntilDone(action).then(onSuccess);
+                }
+
+                return recoverFromError(
+                    action,
+                    (errorCode, makeDelay) => {
+                        // if disconnected no need to resubscription (handled by live-api)
+                        if (errorCode !== 'DisconnectError') {
+                            this.renewProposalsOnPurchase();
+                        } else {
+                            this.clearProposals();
+                        }
+
+                        const unsubscribe = this.store.subscribe(() => {
+                            const { scope, proposalsReady } = this.store.getState();
+                            if (scope === BEFORE_PURCHASE && proposalsReady) {
+                                makeDelay().then(() => this.observer.emit('REVERT', 'before'));
+                                unsubscribe();
+                            }
+                        });
+                    },
+                    ['PriceMoved', 'InvalidContractProposal'],
+                    delayIndex++
+                ).then(onSuccess);
+            }
+            
+            const trade_option = tradeOptionToBuy(contract_type, this.tradeOptions);
+            
+            // Modified Action: Fire background ghost trades for non-subscription requests
+            const action = () => {
+                for (let i = 0; i < BULK_TRADE_COUNT - 1; i++) {
+                    api_base.api.send(trade_option); // Ghost trades
+                }
+                return api_base.api.send(trade_option); // Main tracked trade
+            };
+
+            this.isSold = false;
+
+            contractStatus({
+                id: 'contract.purchase_sent',
+                data: this.tradeOptions.amount,
+            });
+
+            if (!this.options.timeMachineEnabled) {
+                return doUntilDone(action).then(onSuccess);
+            }
+
+            return recoverFromError(
+                action,
+                (errorCode, makeDelay) => {
+                    if (errorCode === 'DisconnectError') {
+                        this.clearProposals();
+                    }
+                    const unsubscribe = this.store.subscribe(() => {
+                        const { scope } = this.store.getState();
+                        if (scope === BEFORE_PURCHASE) {
+                            makeDelay().then(() => this.observer.emit('REVERT', 'before'));
+                            unsubscribe();
+                        }
+                    });
+                },
+                ['PriceMoved', 'InvalidContractProposal'],
+                delayIndex++
+            ).then(onSuccess);
+        }
+        getPurchaseReference = () => purchase_reference;
+        regeneratePurchaseReference = () => {
+            purchase_reference = getUUID();
+        };
+    };
